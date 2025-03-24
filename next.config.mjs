686a61c32227885ceee44eac49044cad856e1e2a
devBernardo-Mendes <devbernardo.reis@gmail.com>
2025-03-24 12:39:52 -0300

    feat: :sparkles: Create layout for side bar and config libs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test instanceof RegExp && rule.test.test(".svg")
+    );
+
+    if (!fileLoaderRule) {
+      throw new Error("Regra de carregamento de arquivos não encontrada.");
+    }
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...(fileLoaderRule.resourceQuery?.not || []), /url/],
+        },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
 
 export default nextConfig;
